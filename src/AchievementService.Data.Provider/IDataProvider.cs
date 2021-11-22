2f625c98b1837dcbd94dcdf33e5b99d8de4f81f3
Vladislav <spicin2274@gmail.com>
2021-11-22 16:34:21 +0300

    fixes
    * fixed IDataProvider interface
    * fixed connection with CommonConfigurationTargets

--- src/AchievementService.Data.Provider/IDataProvider.cs
+++ src/AchievementService.Data.Provider/IDataProvider.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Database;
+using LT.DigitalOffice.Kernel.Enums;
 
-namespace AchievementService.Data.Provider
+namespace LT.DigitalOffice.AchievementService.Data.Provider
 {
-    public interface IDataProvider
+    [AutoInject(InjectType.Scoped)]
+    public interface IDataProvider : IBaseDataProvider
     {
     }
 }
