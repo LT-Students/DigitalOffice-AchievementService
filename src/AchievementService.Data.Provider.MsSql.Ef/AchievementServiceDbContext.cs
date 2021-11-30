f762dcafff241ec5a67006ae3e191137ad2d1c36
VladSpitsin <63400560+VladSpitsin@users.noreply.github.com>
2021-11-30 19:29:47 +0300

    Add service to Achievement github repository (#1)
    * create achievement service
    
    * add solution "LT.DigitalOffice.AchievementService";
    * add directory "Solution items" and put the following files there: ".dockerignore", ".editorconfig", ".gitignore", "Common.Configurations.targets", "Dockerfile", "main.yml", "README.md"
    * add directory "src" and put the following items:
    application "AchievementService" (add appsettings.json, set Program.cs, Startup.cs), add following DLLs: AchievementService.Broker (add "Consumers" directory), AchievementService.Business (add "Commands" directory), AchievementService.Data (add "Interfaces" directory), AchievementService.Data.Provider (add "IDataProvider" interface), AchievementService.Data.Provider.MsSql.Ef (add "Migrations" directory and "AchievementServiceDbContext" class), AchievementService.Mappers, AchievementService.Models.Db, AchievementService.Models.Dto, AchievementService.Validation
    
    * fix
    
    * fixes
    
    * fixed IDataProvider interface
    * fixed connection with CommonConfigurationTargets
    
    * fixes
    
    * fixed AchievementServiceDbContext class
    * fixed tabs
    
    * many fixes
    
    * some fixes
    
    * fix logging
    
    * fix lines
    
    * fixes
    
    * fix AspNetCore.HealthChecks versions
    * fix AchievementService.Mappers.csproj
    * fix launchSettings.json
    
    * fix empty line

---
+++ src/AchievementService.Data.Provider.MsSql.Ef/AchievementServiceDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LT.DigitalOffice.AchievementService.Data.Provider.MsSql.Ef
+{
+  public class AchievementServiceDbContext : DbContext, IDataProvider
+  {
+    public AchievementServiceDbContext(DbContextOptions<AchievementServiceDbContext> options)
+    : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("LT.DigitalOffice.AchievementService.Models.Db"));
+    }
+
+    public void Save()
+    {
+      SaveChanges();
+    }
+
+    public object MakeEntityDetached(object obj)
+    {
+      Entry(obj).State = EntityState.Detached;
+
+      return Entry(obj).State;
+    }
+
+    public void EnsureDeleted()
+    {
+      Database.EnsureDeleted();
+    }
+
+    public bool IsInMemory()
+    {
+      return Database.IsInMemory();
+    }
+
+    public async Task SaveAsync()
+    {
+      await SaveChangesAsync();
+    }
+  }
+}
---
+++ src/AchievementService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -0,0 +1,29 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Common;
+using LT.DigitalOffice.Models.Broker.Requests.Image;
+using LT.DigitalOffice.Models.Broker.Requests.User;
+
+namespace LT.DigitalOffice.AchievementService.Models.Dto.Configurations
+{
+  public class RabbitMqConfig : BaseRabbitMqConfig
+  {
+    // user
+
+    [AutoInjectRequest(typeof(IGetUsersDataRequest))]
+    public string GetUsersDataEndpoint { get; set; }
+
+    [AutoInjectRequest(typeof(ICheckUsersExistence))]
+    public string CheckUsersExistenceEndpoint { get; set; }
+
+    // common
+
+    [AutoInjectRequest(typeof(IDisactivateUserRequest))]
+    public string DisactivateUserEndpoint { get; set; }
+
+    // image
+
+    [AutoInjectRequest(typeof(IGetImagesRequest))]
+    public string GetImagesEndpoint { get; set; }
+  }
+}
